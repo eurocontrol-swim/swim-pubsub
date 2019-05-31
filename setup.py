574b80b2eb0bf391c342d352539f25bda28ad7a4
Alexandros Ntavelos <alexandros.ntavelos.ext@eurocontrol.int>
2019-05-31 13:53:49 +0200

    bump version

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ __author__ = 'EUROCONTROL (SWIM)'
 
 setup(
     name='swim-pubsub',
-    version='0.0.5',
+    version='0.0.6',
     description='SWIM Publish Subscribe mini framework',
     author='EUROCONTROL (SWIM)',
     author_email='',
